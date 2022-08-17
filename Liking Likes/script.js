e5a1ddbdc137d6c88c52d01b8dc42b73506d8bf4
Michael Lewis <ekim198806@gmail.com>
2022-08-16 20:36:44 -0700

    git hub assignment

---
+++ Liking Likes/script.js
@@ -0,0 +1,27 @@
+var count1 = 9;
+var count2 = 12;
+var count3 = 9;
+var countElement1 = document.querySelector(".count1");
+var countElement2 = document.querySelector(".count2");
+var countElement3 = document.querySelector(".count3");
+
+
+
+function add1(){
+    count1++;
+    countElement1.innerText = " " + count1;
+}
+
+function add2(){
+    count2++;
+    countElement2.innerText = " " + count2;
+}
+
+function add3(){
+    count3++;
+    countElement3.innerText = " " + count3;
+}
+
+
+
+
